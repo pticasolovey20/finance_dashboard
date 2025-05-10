ba7f01c76601bf0cace29e04b7e6f7258b18f453
pticasolovey <dy.solovyov.work@gmail.com>
2025-05-11 00:05:39 +0300

    extended user session

--- auth.ts
+++ auth.ts
@@ -2,9 +2,37 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import authConfig from "@/auth.config";
+import { getUserById } from "@/lib/user";
+import { UserRole } from "@prisma/client";
 import { database } from "@/lib/database";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      console.log({ token });
+      return token;
+    },
+  },
+
   adapter: PrismaAdapter(database),
 
   session: {
