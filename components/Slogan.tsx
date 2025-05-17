6ddb1e39ea9e13f31730a978c4130602b1929643
Dmytro Soloviev <dy.solovyov.work@gmail.com>
2025-05-17 11:55:38 +0300

    updated slogan component

--- components/Slogan.tsx
+++ components/Slogan.tsx
@@ -1,14 +1,14 @@
 const Slogan = () => {
   return (
     <p className="font-medium text-5xl lg:text-6xl !leading-[4rem] lg:!leading-[4.5rem] my-10">
-      Your&nbsp;
-      <span className="inline-block text-white text-5xl md:text-6xl rounded-xl px-2 pb-2 bg-blue-500">
+      <span>Your&nbsp;</span>
+      <span className="inline-block text-white rounded-xl px-2 pb-2 bg-blue-500">
         slogan
       </span>
       <br />
-      could be right
+      <span className="whitespace-nowrap">could be right</span>
       <br />
-      here
+      <span className="whitespace-nowrap">here</span>
     </p>
   );
 };
