e2daab0ac01098be3826b017bd63df6a913d2fbc
Dmytro Soloviev <dy.solovyov.work@gmail.com>
2025-05-21 11:53:29 +0300

    updted AccountSettingsSchema

--- schemas/accountSettingsSchema.ts
+++ schemas/accountSettingsSchema.ts
@@ -1,24 +1,40 @@
 import * as zod from "zod";
 
-export const AccountSettingsSchema = zod.object({
-  firstName: zod
-    .string()
-    .min(2, { message: "Minimum length is 2" })
-    .max(30, { message: "Maximum length is 30" }),
+export const AccountSettingsSchema = zod
+  .object({
+    firstName: zod
+      .string()
+      .min(2, { message: "Minimum length is 2" })
+      .max(30, { message: "Maximum length is 30" }),
 
-  lastName: zod.string().min(2, { message: "Minimum length is 2" }),
+    lastName: zod.string().min(2, { message: "Minimum length is 2" }),
 
-  email: zod.string().email({
-    message: "Not a valid email address",
-  }),
+    email: zod.string().email({
+      message: "Not a valid email address",
+    }),
 
-  currentPassword: zod.string().regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/, {
-    message:
-      "Password must be at least 8 english characters, and contain 1 uppercase, 1 lowercase and 1 digit",
-  }),
+    currentPassword: zod
+      .string()
+      .regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/, {
+        message:
+          "Password must be at least 8 english characters, and contain 1 uppercase, 1 lowercase and 1 digit",
+      }),
 
-  newPassword: zod.string().regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/, {
-    message:
-      "Password must be at least 8 english characters, and contain 1 uppercase, 1 lowercase and 1 digit",
-  }),
-});
+    newPassword: zod.string().regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/, {
+      message:
+        "Password must be at least 8 english characters, and contain 1 uppercase, 1 lowercase and 1 digit",
+    }),
+  })
+  .refine(
+    (data) => {
+      if (data.currentPassword === data.newPassword) {
+        return false;
+      }
+
+      return true;
+    },
+    {
+      message: "New password must be different from current password",
+      path: ["newPassword"],
+    }
+  );
